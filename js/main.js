14d90176cee4b18625f9caa1f125e36c2b61ef00
Bo Mercado <rbomercado1@gmail.com>
2022-02-15 16:25:26 -0800

    updating branch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 /* global data */
 /* exported data */
+// var urlInput = document.querySelector('[name="Url"]');
+// urlInput.addEventListener('input', updateUrl);
+
+// function updateUrl(event) {
+
+// }
