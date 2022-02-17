099c7edd3ca50be56d5aa9ac08c0a1d5a8b7d77c
Bo Mercado <rbomercado1@gmail.com>
2022-02-16 21:26:49 -0800

    updated dom tree

--- js/main.js
+++ js/main.js
@@ -26,3 +26,40 @@ function submit(event) {
   img.setAttribute('src', 'images/placeholder-image-square.jpg');
   form.reset();
 }
+
+function renderEntries(formObject) {
+  var uL = document.createElement('ul');
+
+  var rowOne = document.createElement('div');
+  rowOne.className = 'row height';
+  uL.appendChild(rowOne);
+
+  var entryImg = document.createElement('img');
+  entryImg.setAttribute('src', formObject.url);
+  entryImg.className = 'column-half img-marg';
+  rowOne.appendChild(entryImg);
+
+  var halfColumnOne = document.createElement('div');
+  halfColumnOne.className = 'column-half';
+  rowOne.appendChild(halfColumnOne);
+
+  var headingTwo = document.createElement('h2');
+  var title = document.createTextNode(formObject.title);
+  headingTwo.appendChild(title);
+  halfColumnOne.appendChild(headingTwo);
+
+  var paraOne = document.createElement('p');
+  var notes = document.createTextNode(formObject.notes);
+  paraOne.appendChild(notes);
+  paraOne.className = 'entry-font';
+  halfColumnOne.appendChild(paraOne);
+
+  return uL;
+}
+
+var rowSelector = document.querySelector('.entries-here');
+
+for (let i = 0; i < dataEntries.length; i++) {
+  var loopEntries = renderEntries(data[i]);
+  rowSelector.appendChild(loopEntries);
+}
